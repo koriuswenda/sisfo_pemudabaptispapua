1f38e89149680b42146763533bc9f9222365ede7
korius kussa wenda <koriuswenda@gmail.com>
2024-07-13 19:31:00 +0700

    Tambah data gereja seeder

--- database/seeders/GerejaSeeder.php
+++ database/seeders/GerejaSeeder.php
@@ -15,18 +15,148 @@ class GerejaSeeder extends Seeder
     {
         collect([
             [
-                'nama_gereja' => 'Nama Gereja 1',
+                'nama_gereja' => 'Gereja Baptis Kiwone',
                 'wilayah_id' => 1, // lihat id kota/kab di WilayahSeeder
                 'published_at' => now(),
             ],
             [
-                'nama_gereja' => 'Nama Gereja 2',
-                'wilayah_id' => 5, // lihat id kota/kab di WilayahSeeder
+                'nama_gereja' => 'Gereja Baptis Waena',
+                'wilayah_id' => 1, // lihat id kota/kab di WilayahSeeder
                 'published_at' => now(),
             ],
             [
-                'nama_gereja' => 'Nama Gereja 3',
-                'wilayah_id' => 9, // lihat id kota/kab di WilayahSeeder
+                'nama_gereja' => 'Gereja Baptis Kamkey',
+                'wilayah_id' => 1, // lihat id kota/kab di WilayahSeeder
+                'published_at' => now(),
+            ],
+            [
+                'nama_gereja' => 'Gereja Baptis Bukit Zaitun Skayland',
+                'wilayah_id' => 1, // lihat id kota/kab di WilayahSeeder
+                'published_at' => now(),
+            ],
+            [
+                'nama_gereja' => 'Gereja Baptis Siloam Buper',
+                'wilayah_id' => 1, // lihat id kota/kab di WilayahSeeder
+                'published_at' => now(),
+            ],
+            [
+                'nama_gereja' => 'Gereja Baptis Bukit Pengharapan Waena',
+                'wilayah_id' => 1, // lihat id kota/kab di WilayahSeeder
+                'published_at' => now(),
+            ],
+            [
+                'nama_gereja' => 'Gereja Baptis Tiranus Kota raya',
+                'wilayah_id' => 1, // lihat id kota/kab di WilayahSeeder
+                'published_at' => now(),
+            ],
+            [
+                'nama_gereja' => 'Gereja Baptis Btn Kota raya',
+                'wilayah_id' => 1, // lihat id kota/kab di WilayahSeeder
+                'published_at' => now(),
+            ],
+            [
+                'nama_gereja' => 'Gereja Baptis Obelom Wone',
+                'wilayah_id' => 1, // lihat id kota/kab di WilayahSeeder
+                'published_at' => now(),
+            ],
+            [
+                'nama_gereja' => 'Gereja Baptis Damai Wahno',
+                'wilayah_id' => 1, // lihat id kota/kab di WilayahSeeder
+                'published_at' => now(),
+            ],
+            [
+                'nama_gereja' => 'Gereja Baptis Yohanes Entrop',
+                'wilayah_id' => 1, // lihat id kota/kab di WilayahSeeder
+                'published_at' => now(),
+            ],
+            [
+                'nama_gereja' => 'Gereja Baptis Dawer Entrop',
+                'wilayah_id' => 1, // lihat id kota/kab di WilayahSeeder
+                'published_at' => now(),
+            ],
+            [
+                'nama_gereja' => 'Gereja Baptis Mula-Mula Santa Rosa',
+                'wilayah_id' => 1, // lihat id kota/kab di WilayahSeeder
+                'published_at' => now(),
+            ],
+            [
+                'nama_gereja' => 'Gereja Baptis Sion Angkasa',
+                'wilayah_id' => 1, // lihat id kota/kab di WilayahSeeder
+                'published_at' => now(),
+            ],
+            [
+                'nama_gereja' => 'Gereja Baptis BaseG Pasir II ',
+                'wilayah_id' => 1, // lihat id kota/kab di WilayahSeeder
+                'published_at' => now(),
+            ],
+            [
+                'nama_gereja' => 'Gereja Baptis Kota Barat',
+                'wilayah_id' => 1, // lihat id kota/kab di WilayahSeeder
+                'published_at' => now(),
+            ],
+            [
+                'nama_gereja' => 'Gereja Baptis Moso Batas ',
+                'wilayah_id' => 1, // lihat id kota/kab di WilayahSeeder
+                'published_at' => now(),
+            ],
+            [
+                'nama_gereja' => 'Gereja Baptis Tiofon Koya ',
+                'wilayah_id' => 1, // lihat id kota/kab di WilayahSeeder
+                'published_at' => now(),
+            ],
+            [
+                'nama_gereja' => 'Gereja Baptis Imanuel Toladan ',
+                'wilayah_id' => 1, // lihat id kota/kab di WilayahSeeder
+                'published_at' => now(),
+            ],
+            [
+                'nama_gereja' => 'Gereja Baptis Yahim Sentani ',
+                'wilayah_id' => 1, // lihat id kota/kab di WilayahSeeder
+                'published_at' => now(),
+            ],
+            [
+                'nama_gereja' => 'Gereja Baptis Cahaya Kasih ',
+                'wilayah_id' => 1, // lihat id kota/kab di WilayahSeeder
+                'published_at' => now(),
+            ],
+            [
+                'nama_gereja' => 'Gereja Baptis Walibu Kehiran ',
+                'wilayah_id' => 1, // lihat id kota/kab di WilayahSeeder
+                'published_at' => now(),
+            ],
+            [
+                'nama_gereja' => 'Gereja Baptis Ujung Bumi',
+                'wilayah_id' => 2, // lihat id kota/kab di WilayahSeeder
+                'published_at' => now(),
+            ],
+            [
+                'nama_gereja' => 'Gereja Baptis Jerusalem Keerom',
+                'wilayah_id' => 2, // lihat id kota/kab di WilayahSeeder
+                'published_at' => now(),
+            ],
+            [
+                'nama_gereja' => 'Gereja Baptis Allah Ninom Keerom',
+                'wilayah_id' => 2, // lihat id kota/kab di WilayahSeeder
+                'published_at' => now(),
+            ],
+            [
+                'nama_gereja' => 'Gereja Baptis Getzemani',
+                'wilayah_id' => 2, // lihat id kota/kab di WilayahSeeder
+                'published_at' => now(),
+            ],
+            [
+                'nama_gereja' => 'Gereja Baptis DKI Yahukimo',
+                'wilayah_id' => 3, // lihat id kota/kab di WilayahSeeder
+                'published_at' => now(),
+            ],
+            [
+                'nama_gereja' => 'Gereja Baptis Batra Bonto',
+                'wilayah_id' => 3, // lihat id kota/kab di WilayahSeeder
+                'published_at' => now(),
+            ],
+            [
+                'nama_gereja' => 'Gereja Baptis Sumahai',
+                'wilayah_id' => 3, // lihat id kota/kab di WilayahSeeder
                 'published_at' => now(),
             ],
 
