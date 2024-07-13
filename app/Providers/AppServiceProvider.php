0a55a38fa398c7f72bfa10ffde982d1b6cc25dff
Janzen Faidiban <janzenfaidiban@gmail.com>
2024-07-13 21:31:19 +0900

    update logo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-        View::share('siteTitle', 'Sistem Informasi Pemuda Baptis Papua');
-        View::share('siteDescription', 'Sistem Informasi Pemuda Baptis Papua');
+        View::share('siteTitle', 'SISTEM INFORMASI PEMUDA BAPTIS PAPUA');
+        View::share('siteDescription', 'Wilayah Jayapura Keerom Yahukimo');
     }
 }
